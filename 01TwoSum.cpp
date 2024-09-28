6c79b9df1896bee5aab4a2a6f7561443fe7dde3e
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2024-09-28 23:49:11 +0530

    Optimized Approach code added

--- 01TwoSum.cpp
+++ 01TwoSum.cpp
@@ -1,16 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Brute Force Approach
+
+// vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> res;
+//         for(int i=0 ; i<nums.size()-1; i++){
+//             for(int j=i+1; j<nums.size(); j++){
+//                 if(nums[i]+nums[j]==target){
+//                     res.push_back(i);
+//                     res.push_back(j);
+                    
+//                 }
+//             }
+//         }
+//         return res;
+        
+//     }
+
+// Optimized Approach
+
 vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
-        for(int i=0 ; i<nums.size()-1; i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j]==target){
-                    res.push_back(i);
-                    res.push_back(j);
-                    
-                }
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            if(mp.find(target-nums[i])!=mp.end()){
+                res.push_back(mp[target-nums[i]]);
+                res.push_back(i);
+                return res;
             }
+            mp[nums[i]] = i;
         }
         return res;
         
@@ -25,3 +44,7 @@ int main(){
     }
     return 0;
 }
+
+
+
+

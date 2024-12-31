06bb3b8155c2d9ada448c5b3f57316f55c40741e
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2024-12-31 23:32:12 +0530

    first commit

---
+++ 90Sqrt(x).cpp
@@ -0,0 +1,31 @@
+// Sqrt(x)
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0)
+            return 0;
+        int left = 1, right = x;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1))
+                return mid;
+            else if (mid > x / mid)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+};
+
+int main() {
+    Solution sol;
+    int x = 8;
+    cout << sol.mySqrt(x) << endl;
+    return 0;
+}

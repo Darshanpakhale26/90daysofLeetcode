0a51d075b71abaf1dd1ab7ed197b40f5548184a0
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2025-05-12 23:31:26 +0530

    first commit

---
+++ 117Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+TreeNode* sortedArrayToBST(vector<int>& nums, int left, int right) {
+    if (left > right) return nullptr;
+
+    int mid = left + (right - left) / 2;
+    TreeNode* root = new TreeNode(nums[mid]);
+
+    root->left = sortedArrayToBST(nums, left, mid - 1);
+    root->right = sortedArrayToBST(nums, mid + 1, right);
+
+    return root;
+}
+
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+    return sortedArrayToBST(nums, 0, nums.size() - 1);
+}
+
+// Utility function to print preorder traversal
+void preorder(TreeNode* root) {
+    if (root == nullptr) return;
+    cout << root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    vector<int> nums = {-10, -3, 0, 5, 9};
+    TreeNode* root = sortedArrayToBST(nums);
+
+    cout << "Preorder traversal of BST: ";
+    preorder(root);
+    cout << endl;
+
+    return 0;
+}
